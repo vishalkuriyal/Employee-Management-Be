6a0e707ab8e937437cf45f7bf96c3b7404a6a18f
Ayush Semwal <ayushsemwal648@gmail.com>
2025-12-04 23:48:30 -0500

    restored

--- src/routes/employee.ts
+++ src/routes/employee.ts
@@ -1,14 +1,15 @@
 import express from "express"
 import authMiddleWare from "../middleware/authMiddleware.ts"
-import { addEmployee, upload, getEmployees, getEmployee , UpdateEmployee, DeleteEmployee , fetchEmployeesByDepId} from "../controllers/employeeControler.ts"
+import { addEmployee, upload, getEmployees, getEmployee, UpdateEmployee, DeleteEmployee, fetchEmployeesByDepId } from "../controllers/employeeControler.ts"
 
 const router = express.Router()
 
-router.post('/add', authMiddleWare, upload.single('image') , addEmployee)
+router.post('/add', authMiddleWare, upload.single('image'), addEmployee)
 router.get('/', authMiddleWare, getEmployees)
+// Specific routes MUST come before generic :id routes
+router.get('/department/:id', authMiddleWare, fetchEmployeesByDepId)
 router.get('/:id', authMiddleWare, getEmployee)
 router.put('/:id', authMiddleWare, upload.single('image'), UpdateEmployee)
 router.delete('/:id', authMiddleWare, DeleteEmployee)
-router.get('/department/:id', authMiddleWare, fetchEmployeesByDepId)
 
 export default router
